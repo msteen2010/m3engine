372a9583355456386532cafb3af2be7a37648977
mlasota9909 <50820775+mlasota9909@users.noreply.github.com>
2019-06-08 05:47:01 +1000

    re-fix of threded mode pos jonas amendments

--- m3engine_documents.py
+++ m3engine_documents.py
@@ -185,4 +185,5 @@ def deletedocument():
 
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5030')))
+      app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5030')), threaded=True)
+
--- m3engine_dog.py
+++ m3engine_dog.py
@@ -196,4 +196,5 @@ def searchvaccination():
 
 #Ucomment for unit testing
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=int(os.getenv('PORT', '5015')))
+      app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5015')), threaded=True)
+
